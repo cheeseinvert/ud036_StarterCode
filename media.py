ba09b20df56f34187ad01775b1c90ffd2782bd3c
Adam Grant <adammgrant@gmail.com>
2018-03-21 16:01:55 -0700

    update documentation as per review

--- media.py
+++ media.py
@@ -2,7 +2,15 @@ import webbrowser
 
 
 class Movie:
-    """ Movie class to model movie data """
+    """ Movie class to model movie data
+
+    Attributes:
+        title (str): movie title
+        storyline (str): brief plot description
+        poster_image_url (str): url of the movie poster art
+        trailer_youtube_url (str): url of the movie trailer on youtube
+
+    """
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
